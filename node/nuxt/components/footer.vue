5cd0c6cd7c354a438727deec984b57acd4534c96
Eiguss <roberto.martinez.q@gmail.com>
2022-05-11 01:19:46 +0200

    update

--- node/nuxt/components/footer.vue
+++ node/nuxt/components/footer.vue
@@ -3,50 +3,52 @@
         color="greyB"
         padless
     >
-        <v-row
-            justify="center width-100"
-            no-gutters
-        > 
-            <v-btn
-                v-for="footerItem in footerItems"
-                :key="footerItem.title"
-                color="white"
-                text
-                rounded
-                class="my-2"
-                :to="footerItem.path"
-            >
-                {{ $t('footer.navigation.'+footerItem.title) }}
-            </v-btn>
-        </v-row>
-        <v-row
-            justify="center"
-            no-gutters
-        > 
-            <v-col
-                class="pt-8 pb-6 px-16 white--text"
-                cols="6"
-            >
-                <v-icon> mdi-copyright </v-icon>
-                {{ new Date().getFullYear() }} — {{$t('footer.rightsReserved')}}
-            </v-col>
-            <v-col
-                class="px-16 py-6 white--text text-right"
-                cols="6"
-            >
+        <div class="footer-container">
+            <v-row
+                justify="center width-100"
+                no-gutters
+            > 
                 <v-btn
-                    v-for="item in socialMedia"
-                    :key="item.icon"
-                    class="mx-4 white--text"
-                    :href="item.url"
-                    icon
+                    v-for="footerItem in footerItems"
+                    :key="footerItem.title"
+                    color="white"
+                    text
+                    rounded
+                    class="my-2"
+                    :to="footerItem.path"
                 >
-                    <v-icon size="24px">
-                        {{ item.icon }}
-                    </v-icon>
+                    {{ $t('footer.navigation.'+footerItem.title) }}
                 </v-btn>
-            </v-col>
-        </v-row>
+            </v-row>
+            <v-row
+                justify="center"
+                no-gutters
+            > 
+                <v-col
+                    class="pt-8 pb-6 white--text"
+                    cols="6"
+                >
+                    <v-icon> mdi-copyright </v-icon>
+                    {{ new Date().getFullYear() }} — {{$t('footer.rightsReserved')}}
+                </v-col>
+                <v-col
+                    class="py-6 white--text text-right"
+                    cols="6"
+                >
+                    <v-btn
+                        v-for="item in socialMedia"
+                        :key="item.icon"
+                        class="mx-4 white--text"
+                        :href="item.url"
+                        icon
+                    >
+                        <v-icon size="24px">
+                            {{ item.icon }}
+                        </v-icon>
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </div>
     </v-footer>
 </template>
 
