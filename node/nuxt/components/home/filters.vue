a3054c21fc81fb9ef266fb244d54a3323ea4dae0
Eiguss <roberto.martinez.q@gmail.com>
2022-05-24 19:51:55 +0200

    add date picker logic

---
+++ node/nuxt/components/home/filters.vue
@@ -0,0 +1,124 @@
+<template>
+    <div class="filters">
+        <v-menu
+            v-model="openedFrom"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+        >
+            <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                    v-model="dateFrom"
+                    label="Date range"
+                    prepend-icon="mdi-calendar"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                ></v-text-field>
+            </template>
+            <v-date-picker
+                :allowed-dates="allowedDatesFrom"
+                show-adjacent-months
+                v-model="dateFrom"
+                elevation="5"
+                @input="choosedDateFrom()"
+            ></v-date-picker>
+        </v-menu>
+        <v-menu
+            v-model="openedTo"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+        >
+            <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                    v-model="dateTo"
+                    label="Date range"
+                    prepend-icon="mdi-calendar"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                ></v-text-field>
+            </template>
+            <v-date-picker
+                :allowed-dates="allowedDatesTo"
+                show-adjacent-months
+                v-model="dateTo"
+                elevation="5"
+                @input="openedTo = false"
+            ></v-date-picker>
+        </v-menu>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+    data: () => ({
+        openedFrom: false,
+        openedTo: false,
+        dateFrom: "",
+        dateTo: "",
+    }),
+    watch: {
+        dateFrom() {
+            this.dateTo="";
+        }
+    },
+    methods: {
+        choosedDateFrom(){
+            setTimeout(()=>{
+                this.dateTo = this.getFromPlusOneMonth();
+                this.openedFrom = false;
+                this.openedTo = true;
+            }, 0);
+        },
+        getTodayInt(){
+            let today = new Date();
+            let dd = String(today.getDate()).padStart(2, '0');
+            let mm = String(today.getMonth() + 1).padStart(2, '0');
+            let yyyy = today.getFullYear();
+
+            return parseInt(yyyy+mm+dd);
+        },
+        allowedDatesFrom(val){
+            let valInt = parseInt(val.replace(/-/g, ''), 10);
+            let todayInt = this.getTodayInt();
+
+            return valInt  >= todayInt;
+        },
+        getFromPlusOneMonth(){
+            if(this.dateFrom!==""){
+                let dateFrom = new Date(this.dateFrom);
+                dateFrom = new Date(dateFrom.setMonth(dateFrom.getMonth()+1));
+
+
+                return (dateFrom.getFullYear()
+                    +'-'+("0"+(dateFrom.getMonth()+1)).slice(-2)
+                    +'-'+dateFrom.getDate());
+            }
+
+            return "";
+        },
+        getFromPlusOneMonthInt(){
+            let fromPlusOneMonth = this.getFromPlusOneMonth();
+            if(fromPlusOneMonth!==""){
+                return parseInt(fromPlusOneMonth.replace(/-/g, ''), 10);
+            }
+
+            return "";
+        },
+        allowedDatesTo(val) {
+            let valInt = parseInt(val.replace(/-/g, ''), 10);
+            let limit = this.getFromPlusOneMonthInt();
+            if(limit==""){
+                limit = this.getTodayInt();
+            }
+            
+            return valInt  >= limit;
+        }
+    },
+};
+</script>
--- node/nuxt/pages/index.vue
+++ node/nuxt/pages/index.vue
@@ -1,55 +1,26 @@
 <template>
-    <v-menu
-        v-model="opened"
-        :close-on-content-click="false"
-        transition="scale-transition"
-        offset-y
-        min-width="auto"
-    >
-        <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-                v-model="dateRangeText"
-                label="Date range"
-                prepend-icon="mdi-calendar"
-                readonly
-                v-bind="attrs"
-                v-on="on"
-            ></v-text-field>
-        </template>
-        <v-date-picker
-            :allowed-dates="allowedDates"
-            show-adjacent-months
-            v-model="dates"
-            elevation="5"
-            range
-        ></v-date-picker>
-    </v-menu>
+    <div class="home-page">
+        <div class="home-page__base-image">
+            <v-img
+                max-height="800"
+                min-height="650"
+                :src="require('@/assets/images/home/home.jpg')"
+            >
+                <div class="title-img">
+                    <h1>{{$t('home.title')}}</h1>
+                </div>
+                <filters></filters>
+            </v-img>
+        </div>
+        
+    </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import filters from '~/components/home/filters.vue';
 export default {
-    data: () => ({
-        opened: false,
-        dates: [],
-    }),
-    computed: {
-        dateRangeText () {
-            return this.dates.join(' to  ')
-        },
+    components: {
+        filters,
     },
-    methods: {
-        allowedDates: (val) => {
-            let valInt = parseInt(val.replace(/-/g, ''), 10);
-
-            let today = new Date();
-            let dd = String(today.getDate()).padStart(2, '0');
-            let mm = String(today.getMonth() + 1).padStart(2, '0');
-            let yyyy = today.getFullYear();
-            let todayInt = parseInt(yyyy+mm+dd);
-
-            return valInt  >= todayInt;
-        }
-    },
-};
+}
 </script>
